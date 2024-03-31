4fd170f0a669f9dff0d4e324bc9ead087ab3106d
ALRlasya <135493496+ALRlasya@users.noreply.github.com>
2024-03-31 17:19:10 +0530

    Update constants.js

--- Context/constants.js
+++ Context/constants.js
@@ -1,4 +1,4 @@
 import crowdFunding from "./CrowdFunding.json"
 
-export const CrowdFundingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const CrowdFundingAddress = "0xdf75F6F85E75Fda454F4c2d3E7c33Eb71F93209C";
 export const CrowdFundingABI = crowdFunding.abi;
